64ab4688681a76db8f9c9ca9454c4c5293029626
Sanjana B U <160535331+sanju51@users.noreply.github.com>
2025-02-28 16:05:04 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}){
     let [city , setCity]= useState("");
     let [error , setError]= useState(false);
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "b2c93f34fc007c4fccc157e231dc664c";
+    const API_KEY = "YOUR_API_KEY";
 
     let getWeatherInfo=async() =>{
         try{
